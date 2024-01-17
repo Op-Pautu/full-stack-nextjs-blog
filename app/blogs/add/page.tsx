4606d2cf7840218e0b50d33f040e37a6ef0ebf57
Op-Pautu <pautuop@gmail.com>
2024-01-17 14:34:55 +0530

    Add react hook form and sync inputs to state

--- app/blogs/add/page.tsx
+++ app/blogs/add/page.tsx
@@ -1,19 +1,34 @@
 "use client";
 
-import React, { ChangeEvent, useState } from "react";
+import React, { useState, useRef } from "react";
 import { categories } from "@/lib/utils";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { Editor } from "react-draft-wysiwyg";
+import { useForm } from "react-hook-form";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 const BlogAdd = () => {
   const { data: session } = useSession();
   const [imageUrl, setImageUrl] = useState("");
-  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
-    // @ts-ignore
-    const file = e.target.files[0];
-    setImageUrl(URL.createObjectURL(file));
+  // const [imageFile, setImageFile] = useState<File | null>(null);
+  const headingRef = useRef<HTMLHeadingElement | null>(null);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  // const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+  //   // @ts-ignore
+  //   const file = e.target.files[0];
+  //   setImageFile(file);
+  //   setImageUrl(URL.createObjectURL(file));
+  // };
+
+  const handlePost = (data: any) => {
+    console.log("Data, ", data);
   };
 
   return (
@@ -23,7 +38,10 @@ const BlogAdd = () => {
           <span className="mx-3 font-extrabold">Author: </span>
           <span className="font-semibold uppercase">{session?.user?.name}</span>
         </div>
-        <button className="px-6 py-3 font-semibold text-white shadow-xl bg-violet-600 focus:ring-violet-950 rounded-xl hover:bg-violet-700">
+        <button
+          onClick={handleSubmit(handlePost)}
+          className="px-6 py-3 font-semibold text-white shadow-xl bg-violet-600 focus:ring-violet-950 rounded-xl hover:bg-violet-700"
+        >
           Publish
         </button>
       </div>
@@ -37,7 +55,9 @@ const BlogAdd = () => {
         />
       )}
       <h1
+        ref={headingRef}
         contentEditable={true}
+        suppressContentEditableWarning={true}
         className="w-full p-4 mx-auto font-serif text-2xl font-semibold text-center border-none outline-none h-28 focus:border-none"
       >
         Enter Title
@@ -45,8 +65,13 @@ const BlogAdd = () => {
       <div className="flex w-full my-5">
         <input
           type="file"
-          onChange={handleImageChange}
           className="md:w-[500px] sm:w-[300px] m-auto text-slate-900 bg-gray-100 p-4 rounded-xl font-semibold"
+          {...register("image", {
+            required: true,
+            onChange(event) {
+              setImageUrl(URL.createObjectURL(event.target.files[0]));
+            },
+          })}
         />
       </div>
       <div className="flex w-full my-5">
@@ -54,15 +79,18 @@ const BlogAdd = () => {
           type="text"
           placeholder="Location Eg: Delhi, India"
           className="md:w-[500px] sm:w-[300px] m-auto text-slate-900 bg-gray-100 p-4 rounded-xl font-semibold"
+          {...register("location", { required: true })}
         />
       </div>
       <div className="flex w-full my-5">
         <select
-          name="category"
           className="md:w-[500px] sm:w-[300px] m-auto text-slate-900 bg-gray-100 p-4 rounded-xl font-semibold"
+          {...register("category", { required: true })}
         >
           {categories.map((item) => (
-            <option value={item.id}>{item.name}</option>
+            <option value={item.id} key={item.id}>
+              {item.name}
+            </option>
           ))}
         </select>
       </div>
