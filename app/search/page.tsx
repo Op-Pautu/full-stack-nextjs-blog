4b9eac3b7d25e03caa59a8a3ca98e284cceaf3b7
Op-Pautu <pautuop@gmail.com>
2024-01-22 23:44:33 +0530

    Add search functionality

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,14 +1,36 @@
 "use client";
 
+import BlogItem from "@/components/BlogItem";
+import { BlogItemType } from "@/lib/types";
 import { SearchIcon } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 
 const SearchPage = () => {
+  const [blogs, setBlogs] = useState<BlogItemType[]>([]);
   const { register, handleSubmit } = useForm();
 
-  const handleSearch = ({ search }: { search: string }) => {
-    console.log(search);
+  const handleSearch = async ({ search }: { search: string }) => {
+    let str = search;
+    if (search.includes(" ")) {
+      str = search.split(" ").join("%20");
+    }
+
+    toast.loading("Searching", { id: "SEARCH" });
+
+    try {
+      const res = await fetch(
+        `http://localhost:3000/api/blogs/search?title=${str}`,
+        { cache: "no-store" }
+      );
+
+      const data = await res.json();
+      setBlogs(data.blogs);
+      toast.success("Fetched Successfully", { id: "SEARCH" });
+    } catch (error) {
+      toast.error("Fetching Failed", { id: "SEARCH" });
+    }
   };
   return (
     <section className="w-full h-full">
@@ -22,10 +44,16 @@ const SearchPage = () => {
           {...register("search", { required: true })}
         />
         <SearchIcon
+          //@ts-ignore
           onClick={handleSubmit(handleSearch)}
           size={40}
           className="p-2 rounded-full cursor-pointer hover:bg-slate-300"
         />
+      </div>
+      <div className="flex flex-wrap">
+        {blogs?.map((blog) => (
+          <BlogItem key={blog.id} {...blog} />
+        ))}
       </div>
     </section>
   );
